fixed formula

Machine Learning/linearRegression.py
@@ -7,26 +7,20 @@ def lin_reg(inputs, targets, alpha, fp):
     sumZ = 0.
     j = 0
     k = 0
-    
     inputs = np.concatenate((np.ones((np.shape(inputs)[0],1)), inputs), axis=1)
     beta = np.zeros(np.shape(inputs)[1])
-    
+   
     for i in range(1,101):
         for row in inputs:
-            for point in row:
-                sum +=  beta[k] * point
-                k += 1
-            sum -= targets[j]
-            sumZ += np.sum(np.dot(sum, row))
-            k = 0
+            sum += np.dot(row, np.transpose(beta)) - np.array([targets[j], targets[j], targets[j]])
+            sumZ += sum * row
             j+=1
-        beta[0] -= (alpha / np.shape(inputs)[0]) * sum
-        for m in range(1,np.shape(beta)[0]):
-            beta[m] -= (alpha / np.shape(inputs)[0]) * sum
+        beta[0] -= (alpha/ np.shape(inputs)[0]) * sum[0]
+        beta[1:] -= ((alpha/ np.shape(inputs)[0]) * sumZ[1:])
         sum = 0.
         sumZ = 0.
         j = 0
-        
+    
     fp.write(str(alpha) +',' + str(i) + ',')
     for i in range(np.shape(beta)[0]-1):
         fp.write(str(beta[i]) + ',')

Machine Learning/perceptron.py
@@ -7,17 +7,13 @@ fp = open("output1.csv", 'w')
 
 cols = np.shape(data)[0]
 data = np.concatenate((np.ones((cols,1)), data), axis=1)
-
 while 1:
-    for row in data:
-        f = np.sum(np.dot(np.transpose(row[:3]), weights))
-        if f <= 0:
-            f = -1
-        else:
-            f = 1
     
+    for row in data:
+        a = np.dot(row[:3], weights)
+        a = np.where(a > 0, 1, -1)
         for i in range(np.shape(weights)[0]):
-            if f * row[3] <= 0:
+            if a * row[3] <= 0:
                 weights[i] += row[3] * row[i] 
         if weights[2] - weights[1] == 0:
             break