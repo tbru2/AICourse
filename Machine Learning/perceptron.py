301e0b684404163576f6db417a626ae88a06b44a
tbru2 <tbru2@pdx.edu>
2018-08-02 12:29:07 -0700

    changed calculation on the convergence of weights

--- Machine Learning/perceptron.py
+++ Machine Learning/perceptron.py
@@ -2,26 +2,25 @@ import numpy as np
 
 data = np.loadtxt('input1.csv', delimiter=",")
 weights = np.zeros(3)
-
+prevWeights = np.array([float("inf") for i in range(len(weights))])
 fp = open("output1.csv", 'w')
 
 cols = np.shape(data)[0]
 data = np.concatenate((np.ones((cols,1)), data), axis=1)
+
 while 1:
     
     for row in data:
         a = np.dot(row[:3], weights)
         a = np.where(a > 0, 1, -1)
-        for i in range(np.shape(weights)[0]):
-            if a * row[3] <= 0:
-                weights[i] += row[3] * row[i] 
-        if weights[2] - weights[1] == 0:
-            break
+        if a * row[3] <= 0:
+            
+            weights += row[3] * row[:3] 
+            
     fp.write(str(weights[1]) + ',' + str(weights[2])  + ',' + str(weights[0]) + '\n')
-    if weights[2] - weights[1] == 0:
+   
+    if np.array_equal(weights - prevWeights,np.zeros(len(weights))):
         break
-        
+    prevWeights = np.copy(weights)
+  
 fp.close()
-        
-
-        
